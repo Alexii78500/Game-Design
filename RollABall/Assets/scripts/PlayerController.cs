98caae13eb9a8416fa8e63233b248c4f0c4d0ae1
Alexii78500 <apetignat@csumb.edu>
2023-01-29 15:04:44 -0800

    Added Project1
    Contains a Unity project and 2 scenes: Minigame and Solar System

---
+++ RollABall/Assets/scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movx;
+    private float movy;
+    public float speed = 0;
+    private int count;
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI WinText;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        WinText.gameObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementvalue)
+    {
+        Vector2 movementvetcor= movementvalue.Get<Vector2>();
+        
+        movx = movementvetcor.x;
+        movy = movementvetcor.y;
+    }
+
+    void SetCountText()
+    {
+        text.text = "Count: " + count.ToString();
+        if (count >= 12)
+            WinText.gameObject.SetActive(true);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(movx, 0f, movy);
+        rb.AddForce(move * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+}
